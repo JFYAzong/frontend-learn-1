8467997dd6001b3235c8c003e2ef79438ca95a24
JFYAzong <1018720708@qq.com>
2022-07-15 16:19:34 +0800

    Update Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -8,6 +8,23 @@
     </div>
     <div class="border_huodong">
         <div  class="icon_huodong"></div>
+        <div class="text_huodong">活动安排</div>
+    </div>
+    <div class="border_tongzhi">
+        <div class="icon_tongzhi"></div>
+        <div class="text_tongzhi">通知公告</div>
+    </div>
+    <div class="border_fengcai">
+        <div class="icon_fengcai"></div>
+        <div class="text_fengcai">活动风采</div>
+    </div>
+    <div class="border_qiandao">
+        <div class="icon_qiandao"></div>
+        <div class="text_qiandao">签到打卡</div>
+    </div>
+    <div class="border_hujiao">
+        <div class="icon_hujiao"></div>
+        <div class="text_hujiao">呼叫客服</div>
     </div>
     <div class="last"></div>
    </div>
@@ -107,12 +124,156 @@ export default {
     top: 15px;
     left: 29px;
 }
+.text_huodong{
+    position: absolute;
+    top: 89px;
+    left: 17px;
+    width: 96px;
+    height: 33px;
+    font-size: 24px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 600;
+    color: #CC1E1E;
+    line-height: 33px;
+    -webkit-background-clip: text;
+    /* -webkit-text-fill-color: transparent; */
+}
+.border_tongzhi{
+    position: absolute;
+    top: 785px;
+    left: 301px;
+    width: 131px;
+    height: 134px;
+    background: #FFFFFF;
+    border-radius: 15px;
+    border: 2px solid #AF3333;
+}
+.icon_tongzhi{
+    position: absolute;
+    background: url(../assets/活动安排.png);
+    background-size: 100% 100%;
+    width: 72px;
+    height: 72px;
+    top: 15px;
+    left: 29px;
+}
+.text_tongzhi{
+    position: absolute;
+    top: 89px;
+    left: 17px;
+    width: 96px;
+    height: 33px;
+    font-size: 24px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 600;
+    color: #CC1E1E;
+    line-height: 33px;
+    -webkit-background-clip: text;
+}
+.border_fengcai{
+    position: absolute;
+    top: 785px;
+    left: 474px;
+    width: 131px;
+    height: 134px;
+    background: #FFFFFF;
+    border-radius: 15px;
+    border: 2px solid #AF3333;
+}
+.icon_fengcai{
+    position: absolute;
+    background: url(../assets/活动安排.png);
+    background-size: 100% 100%;
+    width: 72px;
+    height: 72px;
+    top: 15px;
+    left: 29px;
+}
+.text_fengcai{
+    position: absolute;
+    top: 89px;
+    left: 17px;
+    width: 96px;
+    height: 33px;
+    font-size: 24px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 600;
+    color: #CC1E1E;
+    line-height: 33px;
+    -webkit-background-clip: text;
+}
+.border_qiandao{
+    position: absolute;
+    top: 785px;
+    left: 647px;
+    width: 131px;
+    height: 134px;
+    background: #FFFFFF;
+    border-radius: 15px;
+    border: 2px solid #AF3333;
+}
+.icon_qiandao{
+    position: absolute;
+    background: url(../assets/活动安排.png);
+    background-size: 100% 100%;
+    width: 72px;
+    height: 72px;
+    top: 15px;
+    left: 29px;
+}
+.text_qiandao{
+    position: absolute;
+    top: 89px;
+    left: 17px;
+    width: 96px;
+    height: 33px;
+    font-size: 24px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 600;
+    color: #CC1E1E;
+    line-height: 33px;
+    -webkit-background-clip: text;
+}
+.border_hujiao{
+    position: absolute;
+    top: 785px;
+    left: 820px;
+    width: 131px;
+    height: 134px;
+    background: #FFFFFF;
+    border-radius: 15px;
+    border: 2px solid #AF3333;
+}
+.icon_hujiao{
+    position: absolute;
+    background: url(../assets/活动安排.png);
+    background-size: 100% 100%;
+    width: 72px;
+    height: 72px;
+    top: 15px;
+    left: 29px;
+}
+.text_hujiao{
+    position: absolute;
+    top: 89px;
+    left: 17px;
+    width: 96px;
+    height: 33px;
+    font-size: 24px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 600;
+    color: #CC1E1E;
+    line-height: 33px;
+    -webkit-background-clip: text;
+}
 .last{
     position: absolute;
-    background-color: yellow;
     top: 1010px;
     left: 115px;
     width: 850px;
     height: 826px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5000);
+    border-radius: 16px;
 }
 </style>
