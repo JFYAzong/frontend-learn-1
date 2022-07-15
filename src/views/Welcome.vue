5ea51b452dec6a2490ca6e91a4a2235330c2c405
JFYAzong <1018720708@qq.com>
2022-07-15 15:34:50 +0800

    Update Welcome.vue

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,10 +1,14 @@
 <template>
    <div class="global">
     <div class="tittle">桐庐老干部局信息系统</div>
+    <div class="swiper">
+        <div class="swiper_border"></div>
+        <div class="swiper_icon"></div>
+        <div class="swiper_text">党建风采</div>
+    </div>
     <div class="border_huodong">
         <div  class="icon_huodong"></div>
     </div>
-    <div class="swiper"></div>
     <div class="last"></div>
    </div>
 </template>
@@ -43,11 +47,46 @@ export default {
 }
 .swiper{
     position: absolute;
-    background-color: aqua;
     top: 274px;
     left: 115px;
     width: 850px;
     height: 434px;
+    background: #F0E0E0;
+    box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5000);
+    border-radius: 16px;
+}
+.swiper_border{
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 95px;
+    width: 850px;
+    height: 339px;
+    background: #FFFFFF;
+}
+.swiper_icon{
+    position: absolute;
+    background: url(../assets/icon.png);
+    background-size: 100% 100%;
+    width: 42px;
+    height: 43px;
+    top: 362px;
+    left: 626px;
+}
+.swiper_text{
+    position: absolute;
+    top: 361px;
+    left: 678px;
+    width: 144px;
+    height: 50px;
+    font-size: 36px;
+    font-family: PingFangSC-Semibold, PingFang SC;
+    font-weight: 600;
+    color: #E13131;
+    line-height: 50px;
+    text-shadow: 0px 2px 2px rgba(0,0,0,0.5000);
+    -webkit-background-clip: text;
+    /* -webkit-text-fill-color: transparent; */
 }
 .border_huodong{
     position: absolute;
