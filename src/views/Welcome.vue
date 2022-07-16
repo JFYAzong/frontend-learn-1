e126ca4af8b17dd4d381ae6cd9850606a9769cb3
illuz <iilluzen@gmail.com>
2022-07-16 11:03:10 +0800

    优化 welcome 样式

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,54 +1,50 @@
 <template>
-   <div class="global">
-    <div class="tittle">桐庐老干部局信息系统</div>
+  <div class="box">
+    <div class="tittle">Xx局信息系统</div>
 
     <div class="swiper">
-        <div class="swiper_border"></div>
-        <div class="swiper_icon"></div>
-        <div class="swiper_text">党建风采</div>
+      <div class="swiper__item"></div>
+      <div class="swiper__icon"></div>
+      <div class="swiper__text">党建风采</div>
     </div>
 
-    <div class="border_huodong">
-        <div  class="icon_huodong"></div>
-        <div class="text_huodong">活动安排</div>
-    </div>
-
-    <div class="border_tongzhi">
-        <div class="icon_tongzhi"></div>
-        <div class="text_tongzhi">通知公告</div>
-    </div>
-
-    <div class="border_fengcai">
-        <div class="icon_fengcai"></div>
-        <div class="text_fengcai">活动风采</div>
-    </div>
-
-    <div class="border_qiandao">
-        <div class="icon_qiandao"></div>
-        <div class="text_qiandao">签到打卡</div>
-    </div>
-
-    <div class="border_hujiao">
-        <div class="icon_hujiao"></div>
-        <div class="text_hujiao">呼叫客服</div>
+    <div class="button-box">
+      <div class="button">
+        <div class="button__icon"></div>
+        <div class="button__text">活动安排</div>
+      </div>
+      <div class="button">
+        <div class="button__icon"></div>
+        <div class="button__text">活动安排</div>
+      </div>
+      <div class="button">
+        <div class="button__icon"></div>
+        <div class="button__text">活动安排</div>
+      </div>
+      <div class="button">
+        <div class="button__icon"></div>
+        <div class="button__text">活动安排</div>
+      </div>
+      <div class="button">
+        <div class="button__icon"></div>
+        <div class="button__text">活动安排</div>
+      </div>
     </div>
 
     <div class="last">
-        <div class="last_text">活动风采</div>
+      <div class="last__text">活动风采</div>
     </div>
-   </div>
-   
+  </div>
 </template>
 
 <script>
 export default {
-    name:'Welcome'
-}
+  name: "Welcome"
+};
 </script>
 
-<style scoped>
-.global{
-
+<style scoped lang="scss">
+.box {
   display: flex;
   flex-direction: column;
   width: 100vw;
@@ -56,248 +52,136 @@ export default {
   background: url("../assets/background.jpg");
   background-size: 100% 100%;
   overflow: hidden;
-}
-.tittle{
-    width: 640px;
-    height: 90px;
+
+  .tittle {
+    position: absolute;
+    top: 155px;
+    left: 0;
+    right: 0;
+
     font-size: 64px;
     font-family: PingFangSC-Semibold, PingFang SC;
     font-weight: 600;
-    color: #E00000;
+    color: #e00000;
     line-height: 90px;
-    text-shadow: 0px 2px 4px rgba(0,0,0,0.5000);
+    text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+    text-align: center;
     /* -webkit-background-clip: text;
     -webkit-text-fill-color: transparent; */
-    position: absolute;
-    top: 155px;
-    left: 221px;
-}
-.swiper{
+  }
+
+  .swiper {
     position: absolute;
     top: 274px;
     left: 115px;
     width: 850px;
     height: 434px;
-    background: #F0E0E0;
-    box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5000);
+    background: #f0e0e0;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
     border-radius: 16px;
-}
-.swiper_border{
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 95px;
-    width: 850px;
-    height: 339px;
-    background: #FFFFFF;
-}
-.swiper_icon{
-    position: absolute;
-    background: url(../assets/icon.png);
-    background-size: 100% 100%;
-    width: 42px;
-    height: 43px;
-    top: 362px;
-    left: 626px;
-}
-.swiper_text{
-    position: absolute;
-    top: 361px;
-    left: 678px;
-    width: 144px;
-    height: 50px;
-    font-size: 36px;
-    font-family: PingFangSC-Semibold, PingFang SC;
-    font-weight: 600;
-    color: #E13131;
-    line-height: 50px;
-    text-shadow: 0px 2px 2px rgba(0,0,0,0.5000);
-    -webkit-background-clip: text;
-    /* -webkit-text-fill-color: transparent; */
-}
-.border_huodong{
+    overflow: hidden;
+
+    &__item {
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 95px;
+      width: 850px;
+      height: 339px;
+      background: #ffffff;
+    }
+
+    &__icon {
+      position: absolute;
+      background: url(../assets/icon.png);
+      background-size: 100% 100%;
+      width: 42px;
+      height: 43px;
+      top: 362px;
+      left: 626px;
+    }
+
+    &__text {
+      position: absolute;
+      top: 361px;
+      left: 678px;
+      width: 144px;
+      height: 50px;
+      font-size: 36px;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      font-weight: 600;
+      color: #e13131;
+      line-height: 50px;
+      text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.5);
+      -webkit-background-clip: text;
+      /* -webkit-text-fill-color: transparent; */
+    }
+  }
+
+  .button-box {
     position: absolute;
     top: 785px;
     left: 128px;
-    width: 131px;
-    height: 134px;
-    background: #FFFFFF;
-    border-radius: 15px;
-    border: 2px solid #AF3333;
-}
-.icon_huodong{
-    position: absolute;
-    background: url(../assets/活动安排.png);
-    background-size: 100% 100%;
-    width: 72px;
-    height: 72px;
-    top: 15px;
-    left: 29px;
-}
-.text_huodong{
-    position: absolute;
-    top: 89px;
-    left: 17px;
-    width: 96px;
-    height: 33px;
-    font-size: 24px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 600;
-    color: #CC1E1E;
-    line-height: 33px;
-    -webkit-background-clip: text;
-    /* -webkit-text-fill-color: transparent; */
-}
-.border_tongzhi{
-    position: absolute;
-    top: 785px;
-    left: 301px;
-    width: 131px;
-    height: 134px;
-    background: #FFFFFF;
-    border-radius: 15px;
-    border: 2px solid #AF3333;
-}
-.icon_tongzhi{
-    position: absolute;
-    background: url(../assets/活动安排.png);
-    background-size: 100% 100%;
-    width: 72px;
-    height: 72px;
-    top: 15px;
-    left: 29px;
-}
-.text_tongzhi{
-    position: absolute;
-    top: 89px;
-    left: 17px;
-    width: 96px;
-    height: 33px;
-    font-size: 24px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 600;
-    color: #CC1E1E;
-    line-height: 33px;
-    -webkit-background-clip: text;
-}
-.border_fengcai{
-    position: absolute;
-    top: 785px;
-    left: 474px;
-    width: 131px;
-    height: 134px;
-    background: #FFFFFF;
-    border-radius: 15px;
-    border: 2px solid #AF3333;
-}
-.icon_fengcai{
-    position: absolute;
-    background: url(../assets/活动安排.png);
-    background-size: 100% 100%;
-    width: 72px;
-    height: 72px;
-    top: 15px;
-    left: 29px;
-}
-.text_fengcai{
-    position: absolute;
-    top: 89px;
-    left: 17px;
-    width: 96px;
-    height: 33px;
-    font-size: 24px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 600;
-    color: #CC1E1E;
-    line-height: 33px;
-    -webkit-background-clip: text;
-}
-.border_qiandao{
-    position: absolute;
-    top: 785px;
-    left: 647px;
-    width: 131px;
-    height: 134px;
-    background: #FFFFFF;
-    border-radius: 15px;
-    border: 2px solid #AF3333;
-}
-.icon_qiandao{
-    position: absolute;
-    background: url(../assets/活动安排.png);
-    background-size: 100% 100%;
-    width: 72px;
-    height: 72px;
-    top: 15px;
-    left: 29px;
-}
-.text_qiandao{
-    position: absolute;
-    top: 89px;
-    left: 17px;
-    width: 96px;
-    height: 33px;
-    font-size: 24px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 600;
-    color: #CC1E1E;
-    line-height: 33px;
-    -webkit-background-clip: text;
-}
-.border_hujiao{
-    position: absolute;
-    top: 785px;
-    left: 820px;
-    width: 131px;
-    height: 134px;
-    background: #FFFFFF;
-    border-radius: 15px;
-    border: 2px solid #AF3333;
-}
-.icon_hujiao{
-    position: absolute;
-    background: url(../assets/活动安排.png);
-    background-size: 100% 100%;
-    width: 72px;
-    height: 72px;
-    top: 15px;
-    left: 29px;
-}
-.text_hujiao{
-    position: absolute;
-    top: 89px;
-    left: 17px;
-    width: 96px;
-    height: 33px;
-    font-size: 24px;
-    font-family: PingFangSC-Medium, PingFang SC;
-    font-weight: 600;
-    color: #CC1E1E;
-    line-height: 33px;
-    -webkit-background-clip: text;
-}
-.last{
+    right: 128px;
+    // background: red;
+    display: flex;
+    justify-content: space-between;
+
+    .button {
+      position: relative;
+      width: 131px;
+      height: 134px;
+      background: #ffffff;
+      border-radius: 15px;
+      border: 2px solid #af3333;
+
+      &__icon {
+        position: absolute;
+        background: url(../assets/活动安排.png);
+        background-size: 100% 100%;
+        width: 72px;
+        height: 72px;
+        top: 15px;
+        left: 29px;
+      }
+
+      &__text {
+        position: absolute;
+        top: 89px;
+        left: 17px;
+        font-size: 24px;
+        font-family: PingFangSC-Medium, PingFang SC;
+        font-weight: 600;
+        color: #cc1e1e;
+        line-height: 33px;
+        -webkit-background-clip: text;
+      }
+    }
+  }
+
+  .last {
     position: absolute;
     top: 1010px;
     left: 115px;
     width: 850px;
     height: 826px;
-    background: #FFFFFF;
-    box-shadow: 0px 2px 4px 0px rgba(0,0,0,0.5000);
+    background: #ffffff;
+    box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.5);
     border-radius: 16px;
-}
-.last_text{
-    position: absolute;
-    top: 32px;
-    left: 329px;
-    width: 192px;
-    height: 67px;
-    font-size: 48px;
-    font-family: PingFangSC-Semibold, PingFang SC;
-    font-weight: 600;
-    color: #E13131;
-    line-height: 67px;
-    text-shadow: 0px 2px 4px rgba(0,0,0,0.5000), 0px 2px 2px rgba(0,0,0,0.5000);
-    -webkit-background-clip: text;
+
+    &__text {
+      position: absolute;
+      top: 32px;
+      left: 329px;
+      font-size: 48px;
+      font-family: PingFangSC-Semibold, PingFang SC;
+      font-weight: 600;
+      color: #e13131;
+      line-height: 67px;
+      text-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5),
+        0px 2px 2px rgba(0, 0, 0, 0.5);
+      -webkit-background-clip: text;
+    }
+  }
 }
 </style>
